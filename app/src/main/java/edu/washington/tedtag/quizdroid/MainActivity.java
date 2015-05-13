0a2e8e405132e6de62ef4b726f8ed0703dd0671e
Ted Tagami <tedtag@uw.edu>
2015-05-12 17:05:41 -0700

    Added new screenshot and updated build

--- app/src/main/java/edu/washington/tedtag/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/tedtag/quizdroid/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
