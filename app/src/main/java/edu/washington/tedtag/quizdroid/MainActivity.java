420fc9e1313fcb888ce567d1c2881508436c5f7d
Ted Tagami <tedtag@uw.edu>
2015-05-19 03:44:04 -0700

    Added alarm service

--- app/src/main/java/edu/washington/tedtag/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/tedtag/quizdroid/MainActivity.java
@@ -1,6 +1,11 @@
 package edu.washington.tedtag.quizdroid;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,11 +28,28 @@ public class MainActivity extends ActionBarActivity {
     public QuizTopicCollection quizData = new QuizTopicCollection();
 
     private ListView categoryList;
+    private PendingIntent alarmIntent = null;
+    BroadcastReceiver alarmReceiver = new BroadcastReceiver() {
+        @Override public void onReceive(final Context c, Intent i) {
+            Toast.makeText(MainActivity.this, getPreferences(c.MODE_PRIVATE).getString("sync_url", "http://tednewardsandbox.site44.com/questions.json"), Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int interval = Integer.parseInt(getPreferences(MODE_PRIVATE).getString("sync_frequency", "5"));
+
+        registerReceiver(alarmReceiver, new IntentFilter("edu.washington.tedtag.QUIZDROID"));
+        AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent();
+        i.setAction("edu.washington.tedtag.QUIZDROID");
+        alarmIntent = PendingIntent.getBroadcast(this, 0, i, 0);
+        am.setRepeating(AlarmManager.RTC,
+                System.currentTimeMillis() + interval * 1000 * 60,
+                interval * 1000 * 60, alarmIntent);
 
         try {
             quizData.generateFromJSON(this.getResources().openRawResource(R.raw.data));
